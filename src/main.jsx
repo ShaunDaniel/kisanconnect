15f528c6498abc4565dd04964dd9682ff8fbbd40
ShaunDaniel <shaun.daniel@science.christuniversity.in>
2024-08-24 20:50:06 +0530

    WIP Page
    work in progress page for production until our first major update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import App from './App.jsx'
 import './index.css'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({config})
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <ChakraProvider theme={theme}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ChakraProvider>
+
 )
